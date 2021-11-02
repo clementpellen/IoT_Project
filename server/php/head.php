9265ae9846b3ddf23da8089b29ea6bf557cbbe7c
Quentin Dstl <qdesautel@gmail.com>
2021-11-02 22:16:44 +0100

    start coté serveur

---
+++ server/php/head.php
@@ -0,0 +1,24 @@
+<?php
+	if(!isset($_SESSION)) { 
+		session_start(); 
+	}
+?>
+
+<!DOCTYPE html>
+
+<html lang="fr" dir="ltr">
+
+<head>
+	<title>Checker</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/reset.css"  type="text/css"/>
+	<link rel="stylesheet" href="css/style.css"  type="text/css"/>
+	<!-- <link rel="icon" href="src/favicon.ico" type="image/x-icon" sizes="128x128"> -->
+
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap" rel="stylesheet">
+</head>
+
+<body>
